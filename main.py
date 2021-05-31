f2a2d2f3706645035f65b46750e60fded57229fd
Gurgen Verdian <gurgen.verdian@gmail.com>
2021-05-31 17:03:04 -0400

    testing

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 import tkinter
 
-print('hello tk')
+# create a window 
+window = tkinter.Tk()
+
+window.title('My First GUI Program')
+window.minsize(width=500, height=300)
+
+#label
+my_label = tkinter.Label(text='I am a Label', font=("Arial", 24, "bold"))
+my_label.pack(side='left') #needed to have the label appear
+
+
+
+
+# keep the window open; must be at end of code
+window.mainloop()
+
+
+
